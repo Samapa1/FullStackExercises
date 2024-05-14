90caf5b4bc951b0ed8b2485c7cd211da3d25cc06
Samapa1 <saija.m.pajari@gmail.com>
2024-05-14 15:35:19 +0300

    osa1 valmiit tehtävät

--- osa1/anekdootit/src/App.jsx
+++ osa1/anekdootit/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+// Lopullinen versio
 
-// tehtava1.13-1.14
+import { useState } from 'react'
 
 const Button = (props) => {
     return (
@@ -22,7 +22,7 @@ const BestAnecdote = (props) => {
   }
 
   return (
-  <div><h1>Anecdote with most votes</h1>
+  <div><h2>Anecdote with most votes</h2>
   {shoWAnecdote()}</div> )
 }
 
@@ -73,7 +73,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
+      <h2>Anecdote of the day</h2>
           <p>{selected}</p>
           <p>has {points[jokenumber]} votes</p>
 
@@ -96,93 +96,4 @@ const App = () => {
 
 export default App
 
-  // return (
-  //   <div>
-  //     <h1>Anecdote of the day</h1>
-  //       <table>
-  //           <tbody>
-  //               <tr>
-  //                   <td>{selected}</td>
-  //               </tr>
-  //               <tr>
-  //                 <td>has {points[jokenumber]} votes</td>
-  //               </tr>
-  //           </tbody>
-  //       </table>
-  //       <table>
-  //           <tbody>
-  //               <tr>
-  //                 <td>
-  //                       <Button
-  //                       handleClick= {vote}
-  //                       text='vote'
-  //                       />
-  //                 </td>
-  //                 <td>
-  //                       <Button
-  //                       handleClick= {randomAnecdote}
-  //                       text='next anecdote'
-  //                       />
-  //                 </td>
-  //               </tr>
-  //           </tbody>
-  //       </table>
-  //   </div>
-        
-    
-  // )
-
-// tehtava 1.12
-// const Button = (props) => {
-//     return (
-//       <button onClick={props.handleClick}>
-//         {props.text}
-//       </button>
-//     )
-//   }
-
-// const App = () => {
-//   const anecdotes = [
-//     'If it hurts, do it more often.',
-//     'Adding manpower to a late software project makes it later!',
-//     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-//     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-//     'Premature optimization is the root of all evil.',
-//     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-//     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-//     'The only way to go fast, is to go well.'
-//   ]
-   
-//   const [selected, setSelected] = useState(anecdotes[0])
-
-//   const randomAnecdote = () => {
-//     let jokenumber  = Math.floor(Math.random() * 8);
-//     setSelected(anecdotes[jokenumber])
-//     console.log(jokenumber)
-//   }
-  
-
-//   return (
-//     <div>
-//         <table>
-//             <tbody>
-//                 <tr>
-//                     <td>
-//                         <Button
-//                         handleClick= {randomAnecdote}
-//                         text='next anecdote'
-//                         />
-//                     </td>
-//                 </tr>
-//                 <tr>
-//                     <td>{selected}</td>
-//                 </tr>
-//             </tbody>
-//         </table>
-//     </div>
-        
-    
-//   )
-// }
-
-// export default App
+ 
